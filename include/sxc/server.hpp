4dfd3ef29c9a10ac74ebf192a681889192bfcc70
thinkty <thinkty@naver.com>
2021-06-26 21:26:32 +0900

    Updated server code with TUI

--- include/sxc/server.hpp
+++ include/sxc/server.hpp
@@ -1,5 +1,18 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+#include "tui.hpp"
+
+#include <string>
+
+class Server
+{
+private:
+	std::wstring m_cmd;
+	TUI m_ui;
+
+public:
+	Server();
+};
 
 #endif
--- src/server.cpp
+++ src/server.cpp
@@ -1,3 +1,12 @@
 #include <sxc/server.hpp>
 
+/**
+ * Initialize the TUI and parse user input
+ */
+Server::Server()
+	: m_cmd{}
+	, m_ui{&m_cmd}
+{
+	m_ui.Init();
+}
 
